5db82b601241c65581210e3514fd5a1eee81de3b
JGolding3953 <j.golding3953@student.leedsmet.ac.uk>
2014-05-15 23:14:13 +0000

    Modify environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+Attractions::Application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
