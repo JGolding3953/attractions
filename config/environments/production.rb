7ee0ee3a99aa8fab475c6c111e5ea0b0c0d4932b
JGolding3953 <j.golding3953@student.leedsmet.ac.uk>
2014-05-15 23:22:37 +0000

    Modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,5 +98,5 @@ Attractions::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 end
