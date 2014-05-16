7b40380afc2e328ca159e648c1c55075207436e5
JGolding3953 <j.golding3953@student.leedsmet.ac.uk>
2014-05-16 01:16:07 +0000

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Attractions::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "young-castle-9029.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
